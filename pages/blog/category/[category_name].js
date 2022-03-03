4f9d47911ff37533c9d8aee30c3891b0c382876f
Richard Stiehm <richardstiehm@gmail.com>
2022-03-03 07:56:34 -0500

    Categories Working

--- pages/blog/category/[category_name].js
+++ pages/blog/category/[category_name].js
@@ -4,24 +4,32 @@ import Link from 'next/link'
 import Layout from "@/components/Layout"
 import matter from 'gray-matter'
 import Post from '@/components/Post'
+import CategoryList from '@/components/CategoryList'
 import {getPosts} from '@/lib/posts'
+import Pagination from '@/components/Pagination'
 
-
-export default function Category({ posts, categoryName }) {
+export default function Category({ posts, categoryName, categories }) {
 
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Posts In {categoryName}</h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => {
-          return <Post key={index} post={post} />
-        })}
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            Posts in {categoryName}
+          </h1>
+
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
-      <Link href="/blog">
-        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
-          All Posts
-        </a>
-      </Link>
     </Layout>
   )
 }
@@ -51,6 +59,11 @@ export async function getStaticProps({ params: { category_name } }) {
   const files = fs.readdirSync(path.join('posts'))
 
   const posts = getPosts()
+   // GET CATS SIDEBAR 
+   const categories = posts.map((post) => {
+    return post.frontmatter.category
+  })
+  const uniqueCategories = [...new Set(categories)]
  
   // FILTER BY CAT 
   const categoryPosts = posts.filter(post => post.frontmatter.category.toLowerCase() === category_name)
@@ -58,7 +71,8 @@ export async function getStaticProps({ params: { category_name } }) {
   return {
     props: {
       posts: categoryPosts,
-      categoryName: category_name
+      categoryName: category_name,
+      categories: uniqueCategories
     }
   }
 }
--- pages/blog/page/[page_index].js
+++ pages/blog/page/[page_index].js
@@ -5,18 +5,30 @@ import Post from '@/components/Post'
 import Pagination from '@/components/Pagination'
 import { POSTS_PER_PAGE } from '@/config/index'
 import {getPosts} from '@/lib/posts'
-
-export default function BlogPage({ posts, numPages, currentPage }) {
+// import Category from '../category/[category_name]'
+import CategoryList from '@/components/CategoryList'
+export default function BlogPage({ posts, numPages, currentPage, categories }) {
 
   return (
     <Layout>
-      <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => {
-          return <Post key={index} post={post} />
-        })}
+      <div className='flex justify-between'>
+        <div className='w-3/4 mr-10'>
+          <h1 className='text-5xl border-b-4 p-5 font-bold'>
+            All Blogs
+          </h1>
+
+          <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {posts.map((post, index) => (
+              <Post key={index} post={post} />
+            ))}
+          </div>
+          <Pagination currentPage={currentPage} numPages={numPages} />
+        </div>
+
+        <div className='w-1/4'>
+          <CategoryList categories={categories} />
+        </div>
       </div>
-      <Pagination currentPage={currentPage} numPages={numPages} />
     </Layout>
   )
 }
@@ -41,7 +53,14 @@ export async function getStaticPaths() {
 export async function getStaticProps({params}) {
   const page = parseInt((params && params.page_index) || 1)
   const files = fs.readdirSync(path.join('posts'))
+
   const posts = getPosts()
+  // GET CATS SIDEBAR 
+  const categories = posts.map((post) => {
+    return post.frontmatter.category
+  })
+  const uniqueCategories = [...new Set(categories)]
+
 
   const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
   const pageIndex = page - 1 
@@ -51,7 +70,8 @@ export async function getStaticProps({params}) {
     props: {
       posts: orderedPosts,
       numPages,
-      currentPage: page
+      currentPage: page,
+      categories: uniqueCategories
     }
   }
 }
